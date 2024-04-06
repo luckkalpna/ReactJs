ef9a12d50ef8aa9984b8127ef455d87413e21c5b
luckkalpna <luckkalpna@gmail.com>
2024-04-06 23:23:07 +0530

    create a NewsApp and also create news items

--- textutils/src/App.js
+++ textutils/src/App.js
@@ -1,15 +1,26 @@
 import './App.css';
 import Navbar from './components/Navbar';
 import TextForm from './components/TextForm';
+import About from './components/About';
+import { BrowserRouter as Router, Switch, Route, Link} from 'react-router-dom';
 
 
 function App() {
   return (
     <>
+    <Router>
     <Navbar titles="TextUtils"/>
     <div className='container'>
-    <TextForm heading="Enter your text here for analyze below"/>
+      <Switch>
+        <Route path="/about">
+          <About />
+        </Route>
+        <Route path="/">
+        <TextForm heading="Enter your text here for analyze below"/>
+        </Route>
+      </Switch>
     </div>
+    </Router>
     </>
   );
 }
---
+++ textutils/src/components/About.js
@@ -0,0 +1,13 @@
+import React from 'react'
+
+function About() {
+return (
+    <div>
+        <h2>This ia an about page</h2>
+        <p>
+Images Tables Figures Forms Overview Form control Select Checks & radios Range Input group Floating labels Layout Validation Components Accordion Alerts Badge Breadcrumb Buttons Button group Card Carousel Close button Collapse Dropdowns List group Modal Navbar Navs & tabs Offcanvas Pagination Placeholders Popovers Progress Scrollspy Spinners Toasts Tooltips Helpers Clearfix Color & background Colored links Focus ring Icon link Position Ratio Stacks Stretched link Text truncation Vertical rule Visually hidden Utilities API Background Borders Colors Display Flex Float Interactions Link Object fit Opacity Overflow Position Shadows Sizing Spacing Text Vertical align Visibility Z-index Extend Approach Icons About Overview Team Brand License Translations Migration Form controls Give textual form controls like an upgrade with custom styles, sizing, focus states, and more. ads via CarbonGet 10 Free Images From Adobe Stock. Start Now. ads via Carbon On this page Example Sizing Form text Disabled Readonly Readonly plain text File input Color Datalists CSS Sass variables Example  Form controls are styled with a mix of Sass and CSS variables, allowing them to adapt to color modes and support any customization method.</p>
+    </div>
+)
+    }
+
+export default About
